9bdd84bb7957f77bc4ca0b5163f5dc2e8e07053b
Toshtemir Chorshanbiyev <chorshanbiyevtoshtemur708@gmail.com>
2024-09-06 01:11:09 +0500

    Bu ikkinchi loyiha va oddiy vazifalar
    Bu bizning ishimiz !

---
+++ python-02.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 19:52:55 2024
+
+@author: Hp
+"""
+print( 'Salom Python ')
+#Bugun bizda yangi dars  bo'ladi!
+                   # python-04. O'zgaruvchilar va ma'lumot turlari!
+                   #VARIABLE - O'ZGARUVCHI
+
+#ism="Temur"
+#yosh=20
+#print(ism)
+#print(yosh)
+
+ism='temur'
+print(ism)
+ism='muhammad'
+print(ism)
+
+#Arifmetik amallar!
+a=9
+b=56
+c=(a+b)**2
+print(c)
+
+ism_sharif = 'Toshtemir Salimovich'
+print(ism_sharif)
+telefon_raqami='+998939088001'
+print(telefon_raqami)
+                       #UYGA VAZIFA.
+matn_nomi="Hello World!"
+print(matn_nomi)
+xabar='Salom python 2024'
+print(xabar)
+xabar='Salom yangi hayot 2024'
+print(xabar)
+
+#class maxsus kalit so'zdir u o'zgaruvchi bo'la olmaydi!
+#class='Salom TATU'
+#print(class)
+
+radius=5
+pi=3.14
+aylana_yuzi=pi*radius**2
+print("Radiusi" , radius ,"ga teng aylananing yuzi =", aylana_yuzi)
